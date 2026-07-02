7ee9174447bf432a9e83f3105d4ab72cf984628a
vishnu <vsarraf70@gmail.com>
2026-07-02 20:13:39 +0200

    added comments in providers file

--- providers.tf
+++ providers.tf
@@ -4,13 +4,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    #random provider is used to generate random values for resource names
     random = {
         source  = "hashicorp/random"
         version = "~> 3.0"
         }
     }
 }
-
+# this is necessary for azurerm provider to work properly
 provider "azurerm" {
   features {}
 }
