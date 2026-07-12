b62633d255b314504f9cc32872505ca57d5bf65b
vishnu <vsarraf70@gmail.com>
2026-07-12 23:15:59 +0200

    finish vmss initial setup

--- VM-Scale-Set.tf
+++ VM-Scale-Set.tf
@@ -5,6 +5,50 @@ resource "azurerm_orchestrated_virtual_machine_scale_set" "example" {
   
   platform_fault_domain_count = 1 #for better reliability
   sku_name                    = "Standard_B1s"
-
+  instances = 3
   zones = ["1"]
+  
+  user_data_base64 = base64encode(file("user_data.sh"))
+
+  os_profile {
+    linux_configuration {
+      disable_password_authentication = true
+      admin_username                  = "azureuser"
+      admin_ssh_key {
+        username   = "azureuser"
+        public_key = file("./ssh/id_rsa.pub")
+      }
+    }
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      instances
+    ]
+  }
+}
 }
