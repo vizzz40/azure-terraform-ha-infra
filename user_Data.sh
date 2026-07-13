91d76e2890a290f6bf17c839d60e8b1e70e63f82
vishnu <vsarraf70@gmail.com>
2026-07-13 03:35:45 +0200

    changed the user data script to a fastapi server

--- user_Data.sh
+++ user_Data.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y apache2 php curl unzip
+apt-get install -y python3-pip python3-venv
 
 
-rm /var/www/html/index.html
-echo "<?php echo 'hello world! '; ?>" > /var/www/html/index.php
+mkdir -p /opt/backend-api
+cd /opt/backend-api
 
 
-systemctl restart apache2
+python3 -m venv venv
+source venv/bin/activate
+pip install fastapi uvicorn
+
+cat << 'EOF' > main.py
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    hostname = socket.gethostname()
+    return {
+        "status": "ok",
+        "message": "working hello world",
+        "server_instance": hostname
+    }
+EOF
+
+
+nohup uvicorn main:app --host 0.0.0.0 --port 80 > api.log 2>&1 &
